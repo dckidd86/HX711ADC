e82b861cc88f872b245ce6f06b37ff5dac487490
dckidd86 <dckidd86@gmail.com>
2016-11-02 12:12:37 -0600

    Update HX711ADC.h

--- firmware/HX711ADC.h
+++ firmware/HX711ADC.h
@@ -19,7 +19,7 @@ class HX711ADC
 		// define clock and data pin, channel, and gain factor
 		// channel selection is made by passing the appropriate gain: 128 or 64 for channel A, 32 for channel B
 		// gain: 128 or 64 for channel A; channel B works with 32 gain factor only
-		HX711ADC(byte dout, byte pd_sck, byte gain = 128);
+		HX711ADC(uint8_t dout, uint8_t pd_sck, uint8_t gain = 128);
 
 		virtual ~HX711ADC();
 
@@ -31,23 +31,23 @@ class HX711ADC
 		// set the gain factor; takes effect only after a call to read()
 		// channel A can be set for a 128 or 64 gain; channel B has a fixed 32 gain
 		// depending on the parameter, the channel is also set to either A or B
-		void set_gain(byte gain = 128);
+		void set_gain(uint8_t gain = 128);
 
 		// waits for the chip to be ready and returns a reading
 		long read();
 
 		// returns an average reading; times = how many times to read
-		long read_average(byte times = 10);
+		long read_average(uint8_t times = 10);
 
 		// returns (read_average() - OFFSET), that is the current value without the tare weight; times = how many readings to do
-		double get_value(byte times = 1);
+		double get_value(uint8_t times = 1);
 
 		// returns get_value() divided by SCALE, that is the raw value divided by a value obtained via calibration
 		// times = how many readings to do
-		float get_units(byte times = 1);
+		float get_units(uint8_t times = 1);
 
 		// set the OFFSET value for tare weight; times = how many times to read the tare value
-		void tare(byte times = 10);
+		void tare(uint8_t times = 10);
 
 		// set the SCALE value; this value is used to convert the raw data to "human readable" data (measure units)
 		void set_scale(float scale = 1.f);
